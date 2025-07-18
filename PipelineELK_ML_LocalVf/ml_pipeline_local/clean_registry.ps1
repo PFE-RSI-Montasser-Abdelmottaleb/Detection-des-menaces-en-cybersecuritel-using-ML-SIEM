ea30be7d10a278af9b8447099b93a705751e50d7
Detection-des-menaces-en-cybersecuritel-using-ML-SIEM <montasser.chorfi.2023@gmail.com>
2025-07-18 09:40:23 +0100

    Delete PipelineELK_ML_LocalVf directory

--- PipelineELK_ML_LocalVf/ml_pipeline_local/clean_registry.ps1
+++
@@ -1,8 +0,0 @@
-# clean_registry.ps1
-$registryPath = "D:\pfe_iset\IntegrationML_ELK\ELK_Stack_8.8.0\filebeat-8.8.0-windows-x86_64\data\registry"
-if (Test-Path $registryPath) {
-    Remove-Item "$registryPath\*" -Recurse -Force
-    Write-Output "✅ Dossier registry supprimé."
-} else {
-    Write-Output "❌ Dossier registry introuvable."
-}
